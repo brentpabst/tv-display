09d6081a16dd47e9b1d101f4dd91a37bb2c7375b
Brent Pabst <me@brentpabst.com>
2024-09-01 22:14:03 -0400

    Debug statements

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,24 +2,6 @@
 set -x
 BUILD_DIR="/usr/share/nginx/html/assets"
 
-# get_env_value() {
-#     local VAR_NAME="$1"
-#     grep "^${VAR_NAME}=" .env | cut -d '=' -f 2-
-# }
-# while IFS= read -r line || [ -n "$line" ]; do
-#     if [[ $line != \#* ]]; then
-#         VAR_NAME=$(echo "$line" | cut -d '=' -f 1)
-#         VAR_VALUE=$(get_env_value "$VAR_NAME")
-#         if [ ! -z "${!VAR_NAME}" ]; then
-#             VAR_VALUE="${!VAR_NAME}"
-#         fi
-#         for file in $BUILD_DIR/*.js; do
-#             sed -i "s|__${VAR_NAME}__|${VAR_VALUE}|g" "$file"
-#         done
-#     fi
-# done < .env
-# nginx -g "daemon off;"
-
 keys="VITE_CORS_PROXY_URL
 VITE_LATITUDE
 VITE_LONGITUDE
@@ -28,6 +10,10 @@ VITE_WEATHERFLOW_STATION_ID
 VITE_WEATHERFLOW_DEVICE_ID
 VITE_ICAL_SYNC_INTERVAL
 VITE_ICAL_FEED_URLS"
+
+la -la $BUILD_DIR
+
+cat /etc/nginx/nginx.conf
 
 for file in "$BUILD_DIR"/*.js ;
 do
